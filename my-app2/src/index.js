c4c9fb581f5792ac283f9b4589b0a43b90bcf9c8
Makar <makartemlyakov@gmail.com>
2018-11-16 16:36:48 +0300

    Update index.js

--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/configureStore'
 import App from './containers/App' 
+import {Route,BrowserRouter} from 'react-router-dom'
+
+
 
 //import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
-
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+        <Route path ="/" component={App}/>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
